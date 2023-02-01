9f736c090320124ad2014cfc7940253d4d23a671
greeneley <anhchangtoanhoc97@gmail.com>
2023-02-02 06:29:27 +0700

    86. Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I'm the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const Shop = () => {
-  return <div>I'm shop</div>;
+  return <h1>I'm shop</h1>;
 };
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
