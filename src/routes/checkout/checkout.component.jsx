98b5e16435926e9f24ad68b86de84735f2addd75
greeneley <anhchangtoanhoc97@gmail.com>
2023-03-01 06:56:00 +0700

    122. Creating Checkout Page

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,29 @@
+import "./checkout.styles.scss";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
+const Checkout = () => {
+  const { cartItems, addItemToCart } = useContext(CartContext);
+  return (
+    <div>
+      <h1>Checkout page</h1>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+              <br />
+              <span>decrement</span> // TODO: handle decrement quantity function
+              <br />
+              <span onClick={() => addItemToCart(cartItem)}>increment</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
