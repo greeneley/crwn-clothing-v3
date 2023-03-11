ad5dcc8a0f11bb6f12b89261572ee946ef04b2c0
greeneley <anhchangtoanhoc97@gmail.com>
2023-03-11 18:30:55 +0700

    135. Category Page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import "./shop.styles.scss";
 import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../../components/categories-preview/categories-preview.component";
+import Category from "../../components/category/category.component";
+
 const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
